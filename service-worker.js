b76d95c11e4e0ec4f8deefc0a7527ccea2778a33
VladislavChepusov <vladislavchepusov@gmail.com>
2024-06-17 23:06:45 +0300

    test

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'jopa';
+var cacheName = 'primordial-boredom';
 
 /* Start the service worker and cache all of the app's content or use the existing one */
 self.addEventListener('install', function (e) {
